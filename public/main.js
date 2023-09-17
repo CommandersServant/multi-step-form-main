4f3eeec1dd3e7b775c1134a6f1ced6dbd0e25578
rasheed <rasheed.team002@gridirontest.com>
2023-09-17 22:17:02 +0100

    styles last step and prevents loading of form when button clicks

--- public/main.js
+++ public/main.js
@@ -1,7 +1,22 @@
+const allButtons = document.querySelectorAll('button');
 
+allButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        event.preventDefault()
+    });
+});
 
-a = {
-    duration: "yearly",
-    plan: "arcade",
-    addons: [],
+function navigateSteps() {
+    const firstStep = document.querySelector('.user-info-form-container');
+    const secondStep = document.querySelector('.plan-selection-container');
+    const thirdStep = document.querySelector('.pick-add-ons-container');
+    const fourthStep = document.querySelector('.finishing-up-container');
+    const fifthStep = document.querySelector('.thank-you-message-container');
+
+    const allSteps = [firstStep, secondStep, thirdStep, fourthStep, fifthStep];
+    
+
+    console.log(allSteps);
 }
+
+navigateSteps();
