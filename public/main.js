b631336944655d11b99edd05d3c7f6d8ee99a56f
rasheed <rasheed.team002@gridirontest.com>
2023-09-18 12:55:18 +0100

    adds step navigation

--- public/main.js
+++ public/main.js
@@ -2,11 +2,12 @@ const allButtons = document.querySelectorAll('button');
 
 allButtons.forEach(button => {
     button.addEventListener('click', (event) => {
-        event.preventDefault()
+        event.preventDefault();
+        navigateSteps(button);
     });
 });
 
-function navigateSteps() {
+function navigateSteps(button) {
     const firstStep = document.querySelector('.user-info-form-container');
     const secondStep = document.querySelector('.plan-selection-container');
     const thirdStep = document.querySelector('.pick-add-ons-container');
@@ -14,9 +15,36 @@ function navigateSteps() {
     const fifthStep = document.querySelector('.thank-you-message-container');
 
     const allSteps = [firstStep, secondStep, thirdStep, fourthStep, fifthStep];
-    
+    let currentStepIndex = null
+    allSteps.forEach((step, index) => {
+        const computedStyle = getComputedStyle(step);
+        if (computedStyle.display !== 'none') {
+            currentStepIndex = index;
+        }
+    });
 
-    console.log(allSteps);
+    if (button.innerHTML === 'Next Step' || button.innerHTML === 'Confirm') {
+        const currentStep = allSteps[currentStepIndex];
+        const nextStepIndex = currentStepIndex + 1;
+        const nextStep = allSteps[nextStepIndex];
+
+        currentStep.style.display = "none";
+        if (nextStep === fifthStep) {
+            nextStep.style.display = 'flex';
+        } else {
+            nextStep.style.display = 'block';
+        }
+    }
+
+    if (button.innerHTML === 'Go Back') {
+        const currentStep = allSteps[currentStepIndex];
+        const previousStepIndex = currentStepIndex - 1;
+        const previousStep = allSteps[previousStepIndex];
+
+        currentStep.style.display = "none";
+        previousStep.style.display = 'block';
+    }
+    console.log(currentStepIndex);
 }
 
-navigateSteps();
+// navigateSteps();
